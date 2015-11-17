b181ad306497bebf32c9786a0a59f8c95d24a85d
chchatham <chathach@gmail.com>
2015-11-17 07:28:29 -0500

    Updated analyze script to work with opiate comments

--- stimscrape_analyze.R
+++ stimscrape_analyze.R
@@ -1,23 +1,66 @@
-#Install necessary pacakges
-list.of.packages <- c("topicmodels","RTextTools")
+rm(list=ls())
+
+#Install necessary packages
+list.of.packages <- c("topicmodels","RTextTools","DescTools")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 
 #user-specified parameters
-numTopics <- 3
-critical_term <- "campaign" #make this heroin
-topNterms <- 10
+maxTopics <- 30 #maximum number of topics to explore
+numTopics <- 10 #number of topics to extract for final run
+critical_term <- "heroin" #make this heroin
+topNterms <- 50
+limitcorpus <- 1000 #limit of number of corpus entries - necessary for memory reasons
 
 #load corpus
-data("NYTimes", package = "RTextTools")
-data <- NYTimes[sample(1:3100,size=1000,replace=FALSE),]
+#data("NYTimes", package = "RTextTools")
+#data <- NYTimes[sample(1:3100,size=3000,replace=FALSE),]
+setwd('C:/Users/CHATHC01/Documents/D3Project/Spivack/Tasks/Stimscrape')
+data <- read.csv('opiates_comments_11-16-15_more.csv')
+
+#data cleaning
+data <- as.character(data$comments)
+data <- data[data != 'deleted '] #removed deleted comments
+data <- data[data != ''] #removed empty comments
+data <- data[sample(1:NROW(data),size=limitcorpus,replace=FALSE)]
+
+data <- substr(data,1,255) #limit to first 255 characters for limitatio in stemming
+strReverse <- function(x)
+  sapply(lapply(strsplit(x, NULL), rev), paste, collapse="")
+data_rev <- strReverse(data)
+data_last_space <- StrPos(data_rev," ")
+data_last_space[is.na(data_last_space)] <- 0
+data_nchar <- nchar(data)
+data <- StrLeft(data,data_nchar-data_last_space)
 
 #create the matrix for the CTM
-matrix <- create_matrix(data$Subject, language="english", removeNumbers=TRUE, stemWords=TRUE, weighting=weightTf)
+matrix <- NULL
+matrix <- create_matrix(data, language="english", removeNumbers=TRUE, stemWords=TRUE, weighting=tm::weightTf)
 
-#Run CTM with number of topics pre-specified
+#remove empty rows
+rowTotals <- apply(matrix , 1, sum) #Find the sum of words in each row
+empty.rows <- matrix[rowTotals == 0, ]$dimnames[1][[1]]
+data <- data[-as.numeric(empty.rows)]
+
+#recreate document term matrix
+matrix <- NULL
+matrix <- create_matrix(data, language="english", removeNumbers=TRUE, stemWords=TRUE, weighting=tm::weightTf)
+
+#Run CTM 
+logliks <- NULL
+for (k in 2:maxTopics){
+  ctm <- NULL
+  ctm <- CTM(matrix, k = k)
+  logliks$k[k] <- k
+  logliks$LL[k] <- sum(ctm@loglikelihood)
+  print(paste('NumTopics: ',k,' LogLik: ',logliks$LL[k],sep=""))
+  plot(logliks$k,logliks$LL, type='l')
+  }
+
+
 ctm <- CTM(matrix, k = numTopics)
+lda <- LDA(matrix, k = numTopics)
 
 #find top N terms for each topic
 ctm_posterior <- posterior(ctm) 
