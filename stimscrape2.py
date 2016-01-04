80c1477babca7aef8b9c9e540096730bbb6e543b
chchatham <chathach@gmail.com>
2016-01-03 21:51:59 -0500

    Happy New Year

--- stimscrape2.py
+++ stimscrape2.py
@@ -5,13 +5,33 @@ import datetime
 import nltk
 nltk.download('punkt')
 nltk.download('stopwords')
-from nltk.corpus import stopwords
+nltk.download('wordnet')
+from nltk.corpus import stopwords, wordnet
+from nltk.stem.wordnet import WordNetLemmatizer
 import string
 import urllib
 import time
+import re
 
 english_stops = stopwords.words('english')
-
+lmtzr = WordNetLemmatizer()
+shortword = re.compile(r'\W*\b\w{1,2}\b')
+nonan = re.compile(r'[^a-zA-Z ]')
+html = re.compile(r'\<[^\>]*\>')
+tag_to_type = {'J': wordnet.ADJ, 'V': wordnet.VERB, 'R': wordnet.ADV}
+def get_wordnet_pos(treebank_tag):
+    return tag_to_type.get(treebank_tag[:1], wordnet.NOUN)
+    
+def clean(text):
+    clean_text = nonan.sub('',html.sub('',text))
+    words = nltk.word_tokenize(shortword.sub('',clean_text.lower()))
+    filtered_words = [w for w in words if not w in english_stops]
+    tags = nltk.pos_tag(filtered_words)
+    return ' '.join(
+        lmtzr.lemmatize(word, get_wordnet_pos(tag[1]))
+        for word, tag in zip(filtered_words, tags)
+    )
+    
 def internet_on():
     try :
         stri = "https://www.google.com"
@@ -23,6 +43,7 @@ def internet_on():
         return False
         
 def tokenize_comment(comment): #converts comment into non-stopword, non-punctuation space-delimited string
+    comment = clean(comment) #lemmatizes
     tokens=nltk.wordpunct_tokenize(comment) #makes individual words from comments
     tokens=[token.lower() for token in tokens if token.lower() not in english_stops] #removes stopwords
     tokens = [token for token in tokens if token not in string.punctuation] #removes punctuation
@@ -45,11 +66,11 @@ def collect_comments(subreddit_string, count, startdate, enddate, freqspec):
     r.login('stimscraper', 'stimscraperpassword',disable_warning='True')
     subreddit = r.get_subreddit(subreddit_string)
 
-    date_range = list(pd.date_range(start=startdate, end=enddate, freq=freqspec)) #populates date range with specified frequency
+    date_range = list(pd.date_range(start=startdate, end=enddate, freq=freqspec)) #[::-1] #populates date range with specified frequency
 
     comment_dict={'authors':[],'comments':[],'links':[],'commenttimes':[]} #where our comments will be stored
 
-    for lower_timestamp, upper_timestamp in zip(date_range, date_range[1:]):
+    for lower_timestamp, upper_timestamp in zip(date_range,date_range[1:]):
         print 'Starting new reddit search query for: ', lower_timestamp
         # Convert timestamps from pd.Timestamp to epoch, offset by 1 second to avoid overlap
         lower_timestamp_epoch = int(lower_timestamp.value / 1e9)
@@ -64,33 +85,33 @@ def collect_comments(subreddit_string, count, startdate, enddate, freqspec):
     
          # Loop through the submissions
         for submission in submissions:
-            print 'Starting on new submission' 
-            try:
-                while True:
-                    if internet_on():
-                        break
-                submission.replace_more_comments(limit=None, threshold=1) #this may take a while...
-                while True:
-                    if internet_on():
-                        break
-                flat_comments = praw.helpers.flatten_tree(submission.comments) #this gives us more than just the top comment
-                for x in flat_comments:
-                    print '.',
-                    x=vars(x)
-                    #print x
-                    author=str(x['author'])
-                    body=str(x['body'].encode('utf8'))
-                    link=str(x['link_id'].encode('utf8'))
-                    commenttime=float(x['created'])
-                    comment_dict['authors'].append(author)
-                    comment_dict['comments'].append(tokenize_comment(body))
-                    comment_dict['links'].append(link)
-                    comment_dict['commenttimes'].append(commenttime)
-                    
-                df = pd.DataFrame.from_dict(comment_dict)
-                df.to_csv('opiates_comments.csv')
-            except Exception:
-                print 'error'
+            print '.', 
+            #try:
+            while True:
+                if internet_on():
+                    break
+            submission.replace_more_comments(limit=None, threshold=1) #this may take a while...
+            while True:
+                if internet_on():
+                    break
+            flat_comments = praw.helpers.flatten_tree(submission.comments) #this gives us more than just the top comment
+            for x in flat_comments:
+                #print '.',
+                x=vars(x)
+                #print x
+                author=str(x['author'])
+                body=str(x['body'].encode('utf8'))
+                link=str(x['link_id'].encode('utf8'))
+                commenttime=float(x['created'])
+                comment_dict['authors'].append(author)
+                comment_dict['comments'].append(tokenize_comment(body))
+                comment_dict['links'].append(link)
+                comment_dict['commenttimes'].append(commenttime)
+                
+            df = pd.DataFrame.from_dict(comment_dict)
+            df.to_csv('opiates_comments.csv')
+            #except Exception:
+            #    print 'error'
     return comment_dict
 
-comment_dict=collect_comments('opiates',None,'11/14/2010','11/16/2015','M')
+comment_dict=collect_comments('opiates',None,'11/01/2014','11/16/2015','M')
